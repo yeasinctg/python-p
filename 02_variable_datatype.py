1a4f06df132e7d4f5787aada384cc475f42df5eb
yeasinctg <yeasinarafatctglohagara@gmail.comm>
2023-05-18 00:18:25 +0600

    variable and data type

---
+++ 02_variable_datatype.py
@@ -0,0 +1,43 @@
+#fruits ='mangoes'
+#print(fruits)
+
+
+
+#rules for naming variable
+#right way
+#fruits ='mangoes'
+#myfav_fruits ='mangoes'
+#_fruits ='mangoes'
+#fruits1 ='mangoes'
+#fruIts1 ='mangoes'
+
+
+
+#wrong way
+#$fruits ='mangoes'
+#myfav fruits ='mangoes'
+#-fruits ='mangoes'
+#1fruits ='mangoes'
+#fruI-ts1 ='mangoes'
+
+
+#data type in python
+
+#1.number 2.string 3.booleans 4.list 5.tupple 6.set 7.dictionaryS
+
+#1. number data type---- integer & floating point
+#integer----10 20 25 125
+X = 21
+print(X)
+
+#floating point--- 10.2 20.5 40.5
+y = 25.5
+print(y)
+
+#boolean--- true or false
+
+z = True
+f = False
+print(z)
+print(f)
+
