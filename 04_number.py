aa936366b59e6b25a2413ba390487e0e86e05fc0
yeasinctg <yeasinarafatctglohagara@gmail.comm>
2023-05-18 12:13:49 +0600

    number py

---
+++ 04_number.py
@@ -0,0 +1,34 @@
+#integer #float
+
+#-------getting the type of numerical values----------
+
+#number = 15
+#number2 = 15.5
+#print(type(number))
+#print(type(number2))
+
+#---------------python number method--------------------
+
+#-- rounding a number--
+#syntax - round(number,ndigits)
+
+#pi = 3.141592653589793238
+
+#two_decimals = round(pi,4)
+#print(two_decimals)
+
+
+#-- raising a number to a power
+#syntax - pow(base,exp)
+
+#x = pow(4,3)
+#print(x)
+
+#--getting absolute value--
+#a = abs(-24)
+#print(a)
+
+
+#--getting quotient and remainder-
+x = divmod(5,2)
+print(x)
