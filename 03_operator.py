69761a3a0b192ed372562204976566f283ce8a8e
yeasinctg <yeasinarafatctglohagara@gmail.comm>
2023-05-18 11:08:45 +0600

    operator py

---
+++ 03_operator.py
@@ -0,0 +1,71 @@
+#------------Arithmetic operator-=----
+#print(10+20)#addition
+#print(10-20)#substraction
+#print(10*20)#multipilication
+#print(10/20)#division
+#print(10**20)#exponentitation
+#print(10%20)#remainder
+#print(10//20)#floar division
+
+#------assignment operator----
+#x = 4
+#y = 3
+#x += y
+#print(x)
+
+#---comparison operator---- true or false
+
+#x = 10
+#y = 20
+#compare = x>y #greater than
+#compare = x<y #less than
+#compare = x>=y
+#compare = x==y #equality
+#compare = x!=y # not equality
+#print(compare)
+
+
+#---logical operator---and,or,not
+#logical and
+#---truth table----- 
+
+#TT = true
+#TF = false
+#FT = false
+#FF = false
+
+#x = 15
+#y = 25
+
+#logical_and = x>y and x==y
+#print(logical_and)
+
+
+#logical or
+#---truth table----- 
+
+#TT = true
+#TF = true
+#FT = true
+#FF = false
+
+#x = 15
+#y = 25
+
+#logical_and = x>y or y<x#
+#print(logical_and)
+
+
+#logical not------ sottoke mitta kre,mittake sotto kre
+
+#x = 35
+#y = 25
+
+#logical_and = not x>y
+#print(logical_and)
+
+#x = 15
+#y = 25
+
+#logical_and = not x>y
+#print(logical_and)
