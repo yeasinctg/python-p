b4e26553f2c29aa4814c39780a9eb9b7dbd4080b
yeasinctg <yeasinarafatctglohagara@gmail.comm>
2023-05-19 22:41:59 +0600

    user input py

---
+++ 06_user_input.py
@@ -0,0 +1,9 @@
+#name = input("Enter your name: ")
+#print("Welcome Mr.",name)
+
+
+#tupe conversion datatype() str() floot() bool()
+num1 = input("Enter number one: ")
+num2 = input("Enter number second: ")
+result = int(num1)+int(num2)
+print(result)
