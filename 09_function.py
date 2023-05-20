b5e42e7157e778905761f4347c0a9692b4226a23
yeasinctg <yeasinarafatctglohagara@gmail.comm>
2023-05-20 23:25:52 +0600

    function py

---
+++ 09_function.py
@@ -0,0 +1,47 @@
+#-------------------------Function  in Python-------------
+#syntex------------
+'''
+
+    def function_name(parameter):
+    #code to be executed
+
+'''
+
+# print hellow horld by creating a function--
+#function declaration
+
+#def hello_world():
+    #functionbody
+    #print("Hello World")
+
+
+# function calling
+#hello_world()
+#hello_world()
+#hello_world()
+
+
+
+
+#function permeter/argument
+#function declaration
+
+#def hello_world(name):
+    #functionbody
+    #x = "Hello " + name
+    #print(x)
+
+
+# function calling
+#hello_world("Ayan")
+
+
+
+
+# program to print addition of two number using function
+def add_number(num1,num2):
+    sum = num1 + num2
+    print(sum)
+
+add_number(10,20)
+
