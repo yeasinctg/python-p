beae6492dba8d4d9ea072ea36b43fedfcf49d1a7
yeasinctg <yeasinarafatctglohagara@gmail.comm>
2023-05-21 01:11:13 +0600

    if statment py

---
+++ 10_if_statment.py
@@ -0,0 +1,38 @@
+#--------------if statment of python------------
+#x = 10
+#y = 20
+#if statment
+#if x<y:
+    #body of if
+    #print("x is smaller than y")
+#elif x>y:
+#body of elife
+    #print("x id gratter than y")
+
+#elif x==y:
+    #print("x is equal")
+#else:
+    #body of else part
+    #print("enter right value")
+
+
+#--program to find out the user is eligible for driving licence or not 
+#user_age = int(input("enter your age: "))
+#if user_age>18 and user_age>=45:
+    #print("you are eligible for driving licence")
+#elif user_age>18 and user_age>=45: 
+     #print("your age to high but you can")
+#else:
+     #print("sorry you are young"40)
+
+
+#---nested if statment--------
+
+x = 15
+if x>5:
+    print("x is more than 5")
+    #nested if
+    if x<10:
+        print("x is more than 10")
+    else:
+        print("x is not more than 5")
